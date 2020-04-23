4ebe0bc2aee590eee56ddfb84c62ce2a21bd982e
Jessica Stone <jstone4815@gmail.com>
2020-04-23 15:22:44 -0700

    add API call to pull in data from axios heroku app

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -6,8 +6,6 @@ import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
-
-
 
 // Postman request for getting all boards: https://inspiration-board.herokuapp.com/boards
 // array of objects. Each obj has a "boards" key with value as object with k-v pairs
@@ -20,11 +18,35 @@ import CARD_DATA from '../data/card-data.json';
 // }
 
 const Board = () => {
+
+  // Modify the Board component to use axios to retrieve card data from the end point, using the board endpoint you configured in the setup requirements.
+
+  const API_URL_BASE = "https://inspiration-board.herokuapp.com/boards/Becca-Jessica/cards"
+  const [cardsList, setCardsList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    axios.get(API_URL_BASE)
+      .then((response) => {        
+        const apiCardsList = response.data.map(card => {
+          return card;
+        });
+        console.log(apiCardsList);
+        
+        
+        setCardsList(apiCardsList);
+      })
+      .catch((error) => {
+        // Still need to handle errors
+        setErrorMessage(error.message);
+      });
+  }, []);
+  
   // console.log(CARD_DATA.cards[0].text);
   // console.log(CARD_DATA);
 
-  const allCards = (CARD_DATA.cards.map((card) => {
-    return <Card text={card.text} emoji={card.emoji}/>
+  const allCards = (cardsList.map((card) => {
+    return <Card key={card.card.id} text={card.card.text} emoji={card.card.emoji}/>
   }))
   
 
