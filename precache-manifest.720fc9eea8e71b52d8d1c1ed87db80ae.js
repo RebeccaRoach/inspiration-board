c4ce978d2efa08708d71784e9fcac36c115eb3ef
RebeccaRoach <rebeccaroach12@gmail.com>
2020-04-26 19:12:49 -0700

    Updates

---
+++ precache-manifest.720fc9eea8e71b52d8d1c1ed87db80ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a003b149c7ac9dfa7e60b6bc29d2ef35",
+    "url": "/inspiration-board/index.html"
+  },
+  {
+    "revision": "0ee466b466911c9d948a",
+    "url": "/inspiration-board/static/css/main.efb3d2e7.chunk.css"
+  },
+  {
+    "revision": "6406eecc4c16d66b6cf1",
+    "url": "/inspiration-board/static/js/2.c638e8e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inspiration-board/static/js/2.c638e8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee466b466911c9d948a",
+    "url": "/inspiration-board/static/js/main.31cb123f.chunk.js"
+  },
+  {
+    "revision": "00bdabaa7c63b64db8f0",
+    "url": "/inspiration-board/static/js/runtime-main.b2c7c5f9.js"
+  }
+]);
